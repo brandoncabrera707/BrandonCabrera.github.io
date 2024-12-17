6469379a50cc487ee5bfe1bef09511b90d90dd24
brandoncabrera707 <brandoncabrera707@gmail.com>
2024-12-17 13:31:15 -0800

    forgot to save

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router,Route,Switch,Link } from "react-router-dom";
+import { BrowserRouter, Routes, Routem,Link } from "react-router-dom";
 import Home from "./pages/home"
 import Experience from "./pages/experience"
 import Project from "./pages/project"
@@ -7,35 +7,13 @@ import Project from "./pages/project"
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/project">Project</Link>
-            </li>
-            <li>
-              <Link to="/experience">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/project">
-            <Project />
-          </Route>
-          <Route path="/experience">
-            <Experience />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects"   element={<Project />} />
+        <Route path="/experience" element={<Experience />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
